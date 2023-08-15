0f730574ee2203ab778d965a0f0d115929a6e370
Adam Wishne <99109711+awishne@users.noreply.github.com>
2023-08-15 14:51:09 -0500

    Style 16x16 grid

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.getElementById('container');
 
 for (let i = 0; i < 16; i++) {
-    for (let j = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
         const div = document.createElement('div');
         div.className = 'grid-div';
         container.appendChild(div);
